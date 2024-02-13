d99dd4bc83a84a7930b15c057fcbf3b42de51f4d
artem <artem.ios.nikitin@gmail.com>
2024-02-13 14:58:37 +0300

    added documentation for VideoCreator

--- SlideShowMaker/Managers/VideoCreator.swift
+++ SlideShowMaker/Managers/VideoCreator.swift
@@ -9,14 +9,23 @@ import AVFoundation
 import Photos
 import UIKit
 
+/// The `VideoCreator` class facilitates the creation of a video by combining a sequence of images.
 final class VideoCreator {
-    private var images: [UIImage]
-    private let frameRate = 5
+    private var images: [UIImage]   // Array of UIImage objects representing frames of the video
+    private let frameRate = 5       // Frames per second
 
+    /// Initializes a `VideoCreator` object with an array of UIImage objects.
+    ///
+    /// - Parameter images: An array of UIImage objects representing frames of the video.
     init(images: [UIImage]) {
         self.images = images
     }
 
+    /// Generates a video by combining provided images with specified video parameters.
+    ///
+    /// - Parameters:
+    ///   - videoInfo: An object of type `VideoInfo` containing information about the video.
+    ///   - completion: A closure called upon completion of video generation, providing a URL to the generated video file or `nil` if an error occurs.
     func createVideo(videoInfo: VideoInfo, completion: @escaping (URL?) -> Void) {
         let outputURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("mov")
 
@@ -82,6 +91,12 @@ final class VideoCreator {
         }
     }
 
+    /// Converts a UIImage object into a CVPixelBuffer with the specified resolution.
+    ///
+    /// - Parameters:
+    ///   - image: The UIImage object to be converted into a pixel buffer.
+    ///   - resolution: An object of type `VideoResolution` specifying the resolution of the video.
+    /// - Returns: A CVPixelBuffer representing the image in the specified resolution.
     private func pixelBuffer(from image: UIImage, resolution: VideoResolution) -> CVPixelBuffer? {
         let options: [String: Any] = [
             kCVPixelBufferCGImageCompatibilityKey as String: true,
@@ -95,9 +110,11 @@ final class VideoCreator {
             return nil
         }
 
+        // Lock the pixel buffer and create a CGContext for drawing.
         CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
         let context = CGContext(data: CVPixelBufferGetBaseAddress(pixelBuffer), width: Int(resolution.size.width), height: Int(resolution.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer), space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
 
+        // Calculate aspect ratio and target dimensions for drawing.
         let aspectRatio = image.size.width / image.size.height
         let targetWidth: CGFloat
         let targetHeight: CGFloat
@@ -111,6 +128,7 @@ final class VideoCreator {
             targetHeight = resolution.size.height
         }
 
+        // Calculate offsets and scaling for centering and resizing.
         let xOffset = (resolution.size.width - targetWidth) / 2
         let yOffset = (resolution.size.height - targetHeight) / 2
 
@@ -120,17 +138,24 @@ final class VideoCreator {
         context?.translateBy(x: xOffset, y: yOffset)
         context?.scaleBy(x: targetWidth / image.size.width, y: targetHeight / image.size.height)
 
+        // Draw the image onto the pixel buffer.
         if let context = context {
             UIGraphicsPushContext(context)
             image.draw(in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
             UIGraphicsPopContext()
         }
 
+        // Unlock the pixel buffer before returning.
         CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
 
         return pixelBuffer
     }
 
+    /// Saves the generated video to the device's photo gallery using the Photos framework.
+    ///
+    /// - Parameters:
+    ///   - url: The URL of the generated video file.
+    ///   - completion: A closure called upon completion of the save operation, providing a URL to the saved video file or `nil` if the operation fails.
     private func saveVideoToGallery(_ url: URL, completion: @escaping (URL?) -> Void) {
         PHPhotoLibrary.shared().performChanges({
             PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: url)
